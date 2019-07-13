aa02d23b464454fb12b2dc948b68154ae92e4372
Kumar <akumar38@contractor.jcp.com>
2019-07-13 12:48:26 -0500

    adding pgsql

---
+++ PivitolCloudFoundry/springboot-postgresql/src/main/java/com/espark/adarsh/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.espark.adarsh.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    private Long empId;
+
+    private String empName;
+
+    public Employee() {
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+}
